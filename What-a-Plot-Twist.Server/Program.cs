f8b7df05490ce7422f6c87d746e9af8a8bd61d93
AMELIA\Ameli <ameliaprzybyl03@gmail.com>
2025-03-20 20:49:45 +0100

    Connction with MongoDB

--- What-a-Plot-Twist.Server/Program.cs
+++ What-a-Plot-Twist.Server/Program.cs
@@ -1,8 +1,11 @@
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-//builder.Services.AddDbContext<ApplicationDbContext>(options =>
-   // options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+// Pobranie Connection String z appsettings.json
+var mongoConnectionString = builder.Configuration.GetConnectionString("MongoDBConnection");
+
+// Rejestracja ustawieñ MongoDB
+builder.Services.Configure<MongoDbSettings>(builder.Configuration.GetSection("MongoDB"));
+builder.Services.AddSingleton<MongoDbContext>();
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
