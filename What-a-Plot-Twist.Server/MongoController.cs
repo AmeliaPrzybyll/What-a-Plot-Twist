2ec105d96a9054f1ffa3ae901823138d6da7a8e4
AMELIA\Ameli <ameliaprzybyl03@gmail.com>
2025-03-20 20:50:02 +0100

    Reapply "Connction with MongoDB"
    This reverts commit f10568f1f7ecffb03400bfe73260a72fb98e1d7e.

---
+++ What-a-Plot-Twist.Server/MongoController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MongoDB.Bson;
+using MongoDB.Driver;
+using System.Threading.Tasks;
+
+[Route("api/mongo")]
+[ApiController]
+public class MongoController : ControllerBase
+{
+    private readonly MongoDbContext _context;
+
+    public MongoController(MongoDbContext context)
+    {
+        _context = context;
+    }
+
+    [HttpPost("add")]
+    public async Task<IActionResult> AddJson([FromBody] object jsonData)
+    {
+        var collection = _context.GetCollection<BsonDocument>("mojaKolekcja");
+        var document = BsonDocument.Parse(jsonData.ToString());
+        await collection.InsertOneAsync(document);
+        return Ok("Dodano JSON do MongoDB");
+    }
+}
